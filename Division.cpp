64661ce815f209834fe63dca21eff945bdc61b9d
GovindVerma <govind.xprt@hotmail.com>
2024-10-20 16:31:40 +0530

    Add division file

---
+++ Division.cpp
@@ -0,0 +1,15 @@
+// Addition.cpp : This file contains the 'main' function. Program execution begins and ends there.
+/**************Addition In C++*****************************/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num1, num2;
+    num1 = 10;
+    num2 = 20;
+    int result = num1 * num2;
+    cout << "Division = " << result << endl;
+
+    cout << "Program terminate" << endl;;
+}
